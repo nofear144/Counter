0969fbee6d7c99f98e74eae163e38bbf27f522cc
nofear144 <nofear144@mail.ru>
2021-08-24 01:40:57 +0300

    Updates

---
+++ precache-manifest.fa145d4601c82d07dc969e45d3fa4ef8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5258df38c9427add2b86ff70582a065a",
+    "url": "/Counter/index.html"
+  },
+  {
+    "revision": "1b4b8517dca49686f91a",
+    "url": "/Counter/static/css/main.90edadb2.chunk.css"
+  },
+  {
+    "revision": "9241cc7c8043029d2689",
+    "url": "/Counter/static/js/2.7a55b710.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Counter/static/js/2.7a55b710.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b4b8517dca49686f91a",
+    "url": "/Counter/static/js/main.9f479af9.chunk.js"
+  },
+  {
+    "revision": "60d75ed92e1196d63d74",
+    "url": "/Counter/static/js/runtime-main.c2fba775.js"
+  }
+]);
